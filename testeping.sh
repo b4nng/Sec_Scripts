5ac96e18b5af41960dfc6fec6afa9afe7f817a97
theN1ghtFury <jafar.untar@gmail.com>
2022-03-06 13:32:18 -0500

    Parallelism on Pingscan

--- testeping.sh
+++ testeping.sh
@@ -5,7 +5,11 @@ then
 	echo Insira primeiro o IP
 	echo Exemplo: $0 10.0.0. 
 else
+	touch pingscan
+	echo "Discovered Hosts:" >> pingscan
 	for i in {1..254};do
-		ping -c1 $1$i | grep "64 bytes" | cut -d " " -f4 | sed 's/://'
-	done	
+		ping -c1 $1$i | grep "64 bytes" | cut -d " " -f4 | sed 's/://' >> pingscan &
+	done
+	cat pingscan
+	rm pingscan	
 fi

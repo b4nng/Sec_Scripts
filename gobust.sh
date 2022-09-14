0447af64ce49120cb525963bc9cc9ce964f45eb6
b4nng <jafar.untar@gmail.com>
2022-09-14 02:17:22 -0400

    gobust added

---
+++ gobust.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+gobuster -t 50 dir --url='http://'$1'/' --wordlist=/usr/share/wordlists/dirb/common.txt -o tempFILE
+_sed='s/\//http:\/\/'$1'\//g'
+cat /tmp/out | grep -v 403 | cut -d ' ' -f1 | sed $_sed
+rm tempFILE

39b4e443d4045a334e1262a457fbfbf46db79ad3
b4nng <jafar.untar@gmail.com>
2023-07-31 21:36:43 -0300

    Changed text and added refused connection exception

--- bannergrab.py
+++ bannergrab.py
@@ -7,8 +7,12 @@ porta=int(sys.argv[2])
 if(len(sys.argv)!=3):
     print("Example usage: ./script IP PORT")
     exit()
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Grabbing",ip,porta)
-s.connect((ip,porta))
-print(s.recv(1024).decode('utf-8'))
-s.close()
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Trying",ip,porta)
+    s.connect((ip,porta))
+    print(s.recv(1024).decode('utf-8'))
+    s.close()
+except ConnectionRefusedError:
+    print(f"[-] Refused on {porta}")
+    pass
